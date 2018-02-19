2c87d117b2d34c58efc63c80d26915901f4809a7
mugi <julio.c.hxh@gmail.com>
2018-02-19 18:15:41 +0100

    Agregados los distintos tipos de usuarios, falta por implementar los metodos get en el modelo

---
+++ GCRS.Web/Controllers/ClientesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GCRS.Data;
+
+namespace GCRS.Web.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(AppDatabase.GetClientes());
+        }
+
+    }
+}
