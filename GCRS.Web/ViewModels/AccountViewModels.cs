6377ae51fc597994337f46450d0317e5c3486dd5
Hieu Do <hieudn.uh@gmail.com>
2018-02-21 22:34:26 -0500

    Mezclar los viewmodels de la autenticacion

--- GCRS.Web/ViewModels/AccountViewModels.cs
+++ GCRS.Web/ViewModels/AccountViewModels.cs
@@ -13,4 +13,27 @@ namespace GCRS.Web.ViewModels
         [Required]
         public string Password { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
