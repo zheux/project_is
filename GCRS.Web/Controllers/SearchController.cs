2890c7a6e0b553bcfb110ec52b1d6eb147bc1b9d
Hieu Do <hieudn.uh@gmail.com>
2018-03-05 10:00:58 -0500

    Anadir controlador para la busqueda alfanumerica

---
+++ GCRS.Web/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GCRS.Web.ViewModels;
+using GCRS.Data;
+
+namespace GCRS.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Rental()
+        {
+            return View(new RentalSearchVM() { Provinces = AppDatabase.GetProvinces(), Municipalities = AppDatabase.GetMunicipalities(), Districts =  AppDatabase.GetDistricts(), Categories = AppDatabase.GetCategories()});
+        }
+
+        // POST: Search
+        [HttpPost]
+        public ActionResult Rental(RentalSearchFilterVM filters)
+        {
+            //TODO: realizar la busqueda alfanumerica
+            return View();
+        }
+    }
+}
---
+++ GCRS.Web/ViewModels/SearchViewModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GCRS.Web.ViewModels
+{
+    public class RentalSearchFilterVM
+    {
+        public string Keywords { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public string SelectedProvince { get; set; }
+
+        public string SelectedMunicipality { get; set; }
+
+        public string SelectedDistrict { get; set; }
+
+        public int MinimumPrice { get; set; }
+
+        public int MaximumPrice { get; set; }
+    }
+
+    public class RentalSearchVM
+    {
+        public RentalSearchFilterVM Filters;
+
+        public IEnumerable<GCRS.Domain.Rental> FilteredRentals;
+
+        public IEnumerable<GCRS.Domain.Province> Provinces;
+        public IEnumerable<GCRS.Domain.Municipality> Municipalities;
+        public IEnumerable<GCRS.Domain.District> Districts;
+        public IEnumerable<GCRS.Domain.Category> Categories;
+    }
+}
