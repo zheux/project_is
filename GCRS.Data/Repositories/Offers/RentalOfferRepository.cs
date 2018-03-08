7868de8fc761c8390318ee9fa73526f599662040
Hieu Do <hieudn.uh@gmail.com>
2018-03-07 23:44:39 -0500

    Implementar los repos de las ofertas

---
+++ GCRS.Data/Repositories/Offers/RentalOfferRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCRS.Domain;
+
+namespace GCRS.Data.Repositories
+{
+    public class RentalOfferRepository : IOfferRepository<RentalOffer>
+    {
+        public void AddOffer(RentalOffer offer)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                if (offer != null)
+                {
+                    context.RentalOffers.Add(offer);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void RemoveOffer(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var offer = context.RentalOffers.SingleOrDefault(m => m.Id == id);
+                if (offer != null)
+                {
+                    context.RentalOffers.Remove(offer);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public RentalOffer FindOffer(Func<RentalOffer, bool> predicate)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.RentalOffers.SingleOrDefault(predicate);
+            }
+        }
+
+        public IEnumerable<RentalOffer> GetOffers()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.RentalOffers.ToList();
+            }
+        }
+    }
+}
---
+++ GCRS.Data/Repositories/Offers/SellOfferRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCRS.Domain;
+
+namespace GCRS.Data.Repositories
+{
+    public class SellOfferRepository : IOfferRepository<SellOffer>
+    {
+        public void AddOffer(SellOffer offer)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                if (offer != null)
+                {
+                    context.SellOffers.Add(offer);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void RemoveOffer(int id)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var offer = context.SellOffers.SingleOrDefault(m => m.Id == id);
+                if (offer != null)
+                {
+                    context.SellOffers.Remove(offer);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public SellOffer FindOffer(Func<SellOffer, bool> predicate)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.SellOffers.SingleOrDefault(predicate);
+            }
+        }
+
+        public IEnumerable<SellOffer> GetOffers()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.SellOffers.ToList();
+            }
+        }
+    }
+}
