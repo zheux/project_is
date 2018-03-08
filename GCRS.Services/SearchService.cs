0ff03b29bf3a448889b80d41ee4813425e9b65fc
Hieu Do <hieudn.uh@gmail.com>
2018-03-08 12:29:30 -0500

    Implementar algunos filtros de la busqueda de alquileres

---
+++ GCRS.Services/SearchService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GCRS.Domain;
+using GCRS.Data.Repositories;
+
+namespace GCRS.Services
+{
+    public class SearchService
+    {
+        private RentalOfferRepository _rentalOfferRepo;
+        private SellOfferRepository _sellOfferRepo;
+
+        public SearchService()
+        {
+            _rentalOfferRepo = new RentalOfferRepository();
+            _sellOfferRepo = new SellOfferRepository();
+        }
+
+        public IEnumerable<RentalOffer> SearchRentalOffers(Func<RentalOffer, bool> cond)
+        {
+            return _rentalOfferRepo.GetOffers()
+                    .Where(m => m.State == State.Published)
+                    .Where(cond)
+                    .ToList();
+        }
+    }
+}
--- GCRS.Web/Controllers/SearchController.cs
+++ GCRS.Web/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using GCRS.Web.ViewModels;
 using GCRS.Domain;
+using GCRS.Services;
 
 namespace GCRS.Web.Controllers
 {
@@ -14,6 +15,7 @@ namespace GCRS.Web.Controllers
         private IMunicipalityRepository _municipalityRepo;
         private IDistrictRepository _districtRepo;
         private ICategoryRepository _categoryRepo;
+        private SearchService _searchService;
 
         public SearchController(IProvinceRepository provinceRepo, IMunicipalityRepository municipalityRepo, IDistrictRepository districtRepo, ICategoryRepository categoryRepo)
         {
@@ -21,12 +23,14 @@ namespace GCRS.Web.Controllers
             _municipalityRepo = municipalityRepo;
             _districtRepo = districtRepo;
             _categoryRepo = categoryRepo;
+            _searchService = new SearchService();
         }
 
         // GET: Search
         public ActionResult Rental()
         {
             return View(new RentalSearchVM() {
+                FilteredRentals = _searchService.SearchRentalOffers(m => m.Id != 0),
                 Provinces = _provinceRepo.GetProvinces(),
                 Municipalities = _municipalityRepo.GetMunicipalities(),
                 Districts = _districtRepo.GetDistricts(),
@@ -38,9 +42,28 @@ namespace GCRS.Web.Controllers
         [HttpPost]
         public ActionResult Rental(RentalSearchFilterVM filters)
         {
-            //TODO: realizar la busqueda alfanumerica
+            Func<RentalOffer, bool> cond = m => {
+                bool ret = true;
+                if (filters.MaximumPrice < filters.MinimumPrice || m.PricePerRTU > filters.MaximumPrice || filters.MinimumPrice > m.PricePerRTU)
+                    ret = false;
+                if (filters.MaximumPrice == 0 && filters.MinimumPrice == 0)
+                    ret = true;
+                if (m.Title.Split().Contains(filters.Keywords))
+                    ret = true;
+                if (filters.SelectedCategory != null && (m.Property.Category == null || m.Property.Category.Name != filters.SelectedCategory))
+                    ret = false;
+                if (filters.SelectedProvince != null && (m.Property.Province == null || m.Property.Province.Name != filters.SelectedProvince))
+                    ret = false;
+                if (filters.SelectedDistrict != null && (m.Property.District == null || m.Property.District.Name != filters.SelectedDistrict))
+                    ret = false;
+                if (filters.SelectedMunicipality != null && (m.Property.Municipality == null || m.Property.Municipality.Name != filters.SelectedMunicipality))
+                    ret = false;
+                return ret;
+            };
+            IEnumerable<RentalOffer> s = _searchService.SearchRentalOffers(cond);
             return View(new RentalSearchVM()
             {
+                FilteredRentals = _searchService.SearchRentalOffers(cond),
                 Provinces = _provinceRepo.GetProvinces(),
                 Municipalities = _municipalityRepo.GetMunicipalities(),
                 Districts = _districtRepo.GetDistricts(),
