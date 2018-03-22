e9e9959669ec00c958a830ad677cd120a02c6794
Hieu Do <hieudn.uh@gmail.com>
2018-03-21 22:59:57 -0400

    Mover los viewmodels de busqueda a la capa de Servicio

--- GCRS.Services/ViewModels/SearchViewModels.cs
+++ GCRS.Services/ViewModels/SearchViewModels.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
 
-namespace GCRS.Web.ViewModels
+namespace GCRS.Services.ViewModels
 {
     public class RentalSearchFilterVM
     {
--- GCRS.Web/Controllers/SearchController.cs
+++ GCRS.Web/Controllers/SearchController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using GCRS.Web.ViewModels;
 using GCRS.Domain;
 using GCRS.Services;
+using GCRS.Services.ViewModels;
 
 namespace GCRS.Web.Controllers
 {
