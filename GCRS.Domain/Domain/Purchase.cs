7c8da22600c1164747c2cc94476f22c93a2751da
Hieu Do <hieudn.uh@gmail.com>
2018-03-21 21:38:03 -0400

    Identificar Llaves primarias para Reservacion y Compra

---
+++ GCRS.Domain/Domain/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCRS.Domain
+{
+    public class Purchase
+    {
+        [Key, Column(Order = 0), ForeignKey("SellOffer")]
+        public int SellOfferId { get; set; }
+        public SellOffer SellOffer { get; set; }
+
+        [Key, Column(Order = 1), ForeignKey("Client")]
+        public string ClientUsername{ get; set; }
+        public Client Client { get; set; }
+
+        public DateTime PurchaseDate{ get; set; }
+    }
+}
---
+++ GCRS.Domain/Domain/RentalReservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCRS.Domain
+{
+    public class RentalReservation
+    {
+        [Key, Column(Order = 0), ForeignKey("RentalOffer")]
+        public int RentalOfferId { get; set; }
+        public RentalOffer RentalOffer { get; set; }
+
+        [Key, Column(Order = 1), ForeignKey("Client")]
+        public string ClientUsername { get; set; }
+        public Client Client { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+}
