7176ce9c038473ea02a7fd77f9447688d5684a57
Hieu Do <hieudn.uh@gmail.com>
2018-03-23 19:44:36 -0400

    Arreglar namespace padre de Tag y anadir interfaz de Repositorio de Tags

--- GCRS.Domain/Domain/Offers.cs
+++ GCRS.Domain/Domain/Offers.cs
@@ -1,5 +1,4 @@
-﻿using GCRS.Domain.Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ GCRS.Domain/IRepositories/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GCRS.Domain
+{
+    public interface ITagRepository
+    {
+        void AddTag(Tag tag);
+        void RemoveTag(int id);
+        Tag FindTag(Func<Tag, bool> predicate);
+        IEnumerable<Tag> GetTags();
+    }
+}
