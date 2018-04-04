6cb3e88fbd872b3812d4dfdf9f03c544e0551d7e
Hieu Do <hieudn.uh@gmail.com>
2018-04-04 18:18:36 -0400

    Asignar el rol correspondiente al usuario logeado

--- GCRS.Web/Global.asax.cs
+++ GCRS.Web/Global.asax.cs
@@ -5,11 +5,23 @@ using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Security.Principal;
+using GCRS.Data.Repositories;
 
 namespace GCRS.Web
 {
     public class Global : System.Web.HttpApplication
     {
+        ClientRepository _clientRepo;
+        AdminRepository _adminRepo;
+        AgentRepository _agentRepo;
+
+        public Global(): base()
+        {
+            _clientRepo = new ClientRepository();
+            _adminRepo = new AdminRepository();
+            _agentRepo = new AgentRepository();
+        }
+
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
             if(HttpContext.Current.User != null)
@@ -23,7 +35,16 @@ namespace GCRS.Web
                         FormsAuthenticationTicket ticket = id.Ticket;
 
                         //TODO: Determinar el rol del usuario usando el 'modelo' para guardarlo
-                        string[] roles = { "admin" };
+                        string[] roles = { "" };
+                        var curUser = HttpContext.Current.User.Identity;
+
+                        if (_adminRepo.FindAdmin(a => a.Username == curUser.Name) != null)
+                            roles[0] = "admin";
+                        else if (_agentRepo.FindAgent(a => a.Username == curUser.Name) != null)
+                            roles[0] = "agent";
+                        else if (_clientRepo.FindClient(c => c.Username == curUser.Name) != null)
+                            roles[0] = "client";
+
                         HttpContext.Current.User = new GenericPrincipal(id, roles);
                     }
                 }
