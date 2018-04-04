49df78fa30cbca5526ff4b74d6faacb4bae67992
Hieu Do <hieudn.uh@gmail.com>
2018-04-04 10:13:13 -0400

    Agregar al dominio el OfferRequest

---
+++ GCRS.Domain/Domain/OfferRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCRS.Domain
+{
+    public enum RequestState { Requested, Draft, Canceled };
+
+    public class OfferRequest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Client")]
+        public string ClientUsername { get; set; }
+        public Client Client { get; set; }
+
+        [ForeignKey("Agent")]
+        public string AgentUsername{ get; set; }
+        public Agent Agent { get; set; }
+
+        public RequestState State { get; set; }
+
+        public DateTime RequestedDate { get; set; }
+    }
+}
---
+++ GCRS.Domain/IRepositories/Offers/IOfferRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GCRS.Domain
+{
+    public interface IOfferRequestRepository
+    {
+        void AddOfferRequest(OfferRequest offer);
+        void RemoveOfferRequest(int id);
+        OfferRequest FindOfferRequest(Func<OfferRequest, bool> predicate);
+        IEnumerable<OfferRequest> GetOfferRequests();
+    }
+}
